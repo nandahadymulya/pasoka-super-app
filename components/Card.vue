ebd986f80da676b327c4b15de8777aec81188ab7
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-06-07 09:44:01 +0700

    feat: reusable card

--- components/Card.vue
+++ components/Card.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <v-card width="200" color="grey lighten-3" flat>
+    <v-card v-bind="$attrs" v-on="$listeners">
       <v-list-item>
         <v-list-item-content class="text-left">
           <v-layout>
-            <v-icon size="48">mdi-folder-open-outline</v-icon>
+            <v-icon size="48">{{ icon }}</v-icon>
             <v-spacer></v-spacer>
             <v-btn icon>
               <v-icon>mdi-dots-vertical</v-icon>
             </v-btn>
           </v-layout>
 
-          <v-list-item-title class="my-2">General</v-list-item-title>
+          <v-list-item-title class="my-2">{{ title }}</v-list-item-title>
           <v-list-item-subtitle>
             <v-layout wrap>
-              <span>Khaira Yasri</span>
+              <span>{{ fieldCount }}</span>
               <v-spacer></v-spacer>
-              <b>Ka. Bag. Umum</b>
+              <b>{{ fieldSize }} MB</b>
             </v-layout>
           </v-list-item-subtitle>
         </v-list-item-content>
@@ -27,5 +27,24 @@
 <script>
 export default {
   name: 'CardComponent',
+  inheritAttrs: false,
+  props: {
+    title: {
+      type: String,
+      default: 'Title',
+    },
+    fieldSize: {
+      type: String,
+      default: '0',
+    },
+    icon: {
+      type: String,
+      default: 'mdi-folder-open-outline',
+    },
+    fieldCount: {
+      type: String,
+      default: '0 file',
+    },
+  },
 }
 </script>
